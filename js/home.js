9b4c1a3c52a29ab72a087ccc6f2a8f017216ebfb
ayana powell <ayanapowell1@gmail.com>
2016-12-16 21:03:10 -0800

    fix margins and sizing on index.html

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){ 
+$(document).ready(function(){
 
 
   $(".background_image").fadeIn(3000);
@@ -6,16 +6,15 @@ $(document).ready(function(){
   setTimeout(function(){
     $("#pop-up").fadeIn("fast");
     $("#pop-up").animate({
-      "width": "450px",
       "height":"200px",
   },700);
   },900);
 
 
   setTimeout(function(){
-   $(".intro").fadeIn("slow"); 
+   $(".intro").fadeIn("slow");
  },1800);
-  
+
   setTimeout(function(){
     console.log(20);
     $(".title").fadeOut("200");
@@ -25,24 +24,24 @@ $(document).ready(function(){
   setTimeout(function(){
     $("#pop-up").animate({
         "height":"0px",
-        "border-style":"none", 
+        "border-style":"none",
         "transition":"0.5s ease"});
-    $(".background_image").fadeOut(900); 
-    },4200); 
+    $(".background_image").fadeOut(900);
+    },4200);
 
   setTimeout(function(){
     $(".intro_slides, .overlay").fadeIn(1500);
   },5100);
 
  setTimeout(function(){
-  $(".nav_city, .about_icon, .fa-facebook, .fa-envelope-o, .fa-instagram").slideDown("slow");
+  $(".nav_navigation, .about_icon, .fa-facebook, .fa-envelope-o, .fa-instagram").slideDown("slow");
   $(".intro_2").fadeIn("800");
  },7100);
- 
+
   var index=0;
   setTimeout(function mySlideshow(event){
     var i;
-    var x=document.getElementsByClassName("my_slides");      
+    var x=document.getElementsByClassName("my_slides");
     for (i=0 ; i < x.length; i++){
       x[i].style.display="none";
     }
@@ -53,13 +52,5 @@ $(document).ready(function(){
 
     },6100);
 
-  
+
 });
-
-		
-  
-
-
-
-
-	
